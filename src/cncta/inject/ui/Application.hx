9fdbaaaebb506f8b02aa7a14339b6347637f5fcb
aszlig <aszlig@redmoonstudios.org>
2012-03-26 22:42:13 +0200

    Get rid of untyped keyword for InitWatch.

--- src/cncta/inject/ui/Application.hx
+++ src/cncta/inject/ui/Application.hx
@@ -3,5 +3,7 @@ package cncta.inject.ui;
 @:native("webfrontend.Application")
 extern class Application implements cncta.inject.qx.IApplication
 {
+    public var initDone:Bool;
+
     public function getChat():Chat;
 }
