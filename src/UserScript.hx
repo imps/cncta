ec5efe4ccbf5379c5442d9733daa47aaa8b54b38
aszlig <aszlig@redmoonstudios.org>
2012-03-28 01:02:00 +0200

    Implement basic AST parsing of types.
    This currently only creates a namespace tree and prints out a trace,
    nothing special yet.

--- src/UserScript.hx
+++ src/UserScript.hx
@@ -1,5 +1,9 @@
 package;
 
+#if macro
+import haxe.macro.Type;
+#end
+
 class UserScript
 {
     public static function main()
@@ -10,14 +14,69 @@ class UserScript
     public static function generator()
     {
         haxe.macro.Compiler.setCustomJSGenerator(
-            function(api) new UserScriptGenerator(api).generate()
+            function(js) new UserScriptGenerator(js).generate()
         );
     }
 #end
 }
 
 #if macro
-class UserScriptGenerator extends haxe.macro.DefaultJSGenerator
+class UserScriptGenerator
 {
+    private var js:haxe.macro.JSGenApi;
+
+    private var packages:userscript.Packages;
+    private var constructor_calls:List<TypedExpr>;
+
+    public function new(js:haxe.macro.JSGenApi)
+    {
+        this.js = js;
+
+        this.packages = new userscript.Packages();
+        this.constructor_calls = new List();
+    }
+
+    private function generate_class(cls:ClassType)
+    {
+        this.packages.add_path(cls.pack.concat([cls.name]));
+
+        if (cls.constructor != null) {
+            //trace(cls.constructor.get());
+            //trace(this.js.generateConstructor(cls.constructor.get().expr));
+        }
+    }
+
+    private function generate_enum(enm:EnumType)
+    {
+        this.packages.add_path(enm.pack.concat([enm.name]));
+    }
+
+    private function create_type(type:Type)
+    {
+        switch (type) {
+            case TInst(clstype, params):
+                var cls = clstype.get();
+                if (cls.init != null)
+                    this.constructor_calls.add(cls.init);
+                if (!cls.isExtern)
+                    this.generate_class(cls);
+            case TEnum(enumtype, params):
+                var enm = enumtype.get();
+                if (!enm.isExtern)
+                    this.generate_enum(enm);
+            default:
+        }
+    }
+
+    public function generate()
+    {
+        for (type in this.js.types) {
+            this.create_type(type);
+        }
+
+        trace(this.packages.toString());
+
+        //trace(this.js.generateConstructor(js.main.constructor.get().expr));
+    }
 }
 #end
