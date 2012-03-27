66ed51ef94540d5d44bbe7fed5eb1b8288c8052b
aszlig <aszlig@redmoonstudios.org>
2012-03-27 02:26:58 +0200

    Turn userscript comments into metadata.
    This currently has no use other than the userscript that will be
    generated won't work anymore, but it's in preparation of our new macro
    based approach of userscript generation.

--- src/CNCTA.hx
+++ src/CNCTA.hx
@@ -1,16 +1,13 @@
-// ==UserScript==
-// @name          IMPS C&C TA Helper
-// @author        aszlig
-// @description   A small tool which helps managing an alliance.
-// @namespace     https://github.com/imps/cncta
-// @include       http://prodgame*.alliances.commandandconquer.com/*/index.*
-// @run_at        document-end
-// @version       0.2.3
-// @license       BSD3
-// @date          2012-03-21
-// ==/UserScript==
-
-class CNCTA
+@name        ("IMPS C&C TA Helper")
+@author      ("aszlig")
+@description ("A small tool which helps managing an alliance.")
+@namespace   ("https://github.com/imps/cncta")
+@include     ("http://prodgame*.alliances.commandandconquer.com/*/index.*")
+@run_at      ("document-end")
+@version     ("0.2.3")
+@license     ("BSD3")
+@date        ("2012-03-21")
+class CNCTA implements UserScript
 {
     private var xmpp:cncta.xmpp.Chat;
 
