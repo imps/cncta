e0fbf0df44963f5f78f4554229d03f7a826f90c1
aszlig <aszlig@redmoonstudios.org>
2012-03-28 14:35:40 +0200

    Change @include to match the new HTTPS URL.

--- src/CNCTA.hx
+++ src/CNCTA.hx
@@ -3,7 +3,7 @@
 // @author        aszlig
 // @description   A small tool which helps managing an alliance.
 // @namespace     https://github.com/imps/cncta
-// @include       http://prodgame*.alliances.commandandconquer.com/*/index.*
+// @include       https://prodgame*.alliances.commandandconquer.com/*/index.*
 // @run_at        document-end
 // @version       0.2.5
 // @license       BSD3
