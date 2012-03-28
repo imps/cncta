233d03ba787636a4cd9759b5205de10b6bcc2021
aszlig <aszlig@redmoonstudios.org>
2012-03-29 00:16:08 +0200

    Bump version number to v0.2.7.

--- src/CNCTA.hx
+++ src/CNCTA.hx
@@ -6,7 +6,7 @@
 // @include       https://prodgame*.alliances.commandandconquer.com/*
 // @include       http://prodgame*.alliances.commandandconquer.com/*
 // @run_at        document-end
-// @version       0.2.6
+// @version       0.2.7
 // @license       BSD3
 // @date          2012-03-21
 // ==/UserScript==
