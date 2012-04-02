2fb2fa05a3d711d55d612a05ec464a5254ac5488
aszlig <aszlig@redmoonstudios.org>
2012-04-02 03:28:11 +0200

    Add a set() method to LayoutItem.
    This is because we currently don't want to implement the whole Qooxdoo object
    hierarchy. For our purposes, abusing LayoutItem for set() seems to be a quite
    acceptable tradeoff.

--- src/cncta/inject/ui/LayoutItem.hx
+++ src/cncta/inject/ui/LayoutItem.hx
@@ -3,4 +3,7 @@ package cncta.inject.ui;
 @:native("qx.ui.core.LayoutItem")
 extern class LayoutItem
 {
+    // this is really in qx.core.Object, but we don't want to
+    // implement a full Qooxdoo binding, do we?
+    public function set(data:Dynamic):LayoutItem;
 }
