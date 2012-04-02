971f4b0c627647e7a0a484b4fa5354b91c9e88a8
aszlig <aszlig@redmoonstudios.org>
2012-04-02 03:57:20 +0200

    Add addListener() to LayoutItem.
    This is for our button to actually have any functionality.

--- src/cncta/inject/qx/LayoutItem.hx
+++ src/cncta/inject/qx/LayoutItem.hx
@@ -3,7 +3,8 @@ package cncta.inject.qx;
 @:native("qx.ui.core.LayoutItem")
 extern class LayoutItem
 {
-    // this is really in qx.core.Object, but we don't want to
+    // these methods really are in qx.core.Object, but we don't want to
     // implement a full Qooxdoo binding, do we?
     public function set(data:Dynamic):LayoutItem;
+    public function addListener(type:String, listener:Void -> Void):String;
 }
