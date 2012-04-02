d7d5ec76ebc2cc11fcc2b47a0bd5646d312e077c
aszlig <aszlig@redmoonstudios.org>
2012-04-02 02:56:53 +0200

    Fix qooxdoo Composite public function types.
    Actually the type for index wasn't wrong but missing altogether.

--- src/cncta/inject/ui/Composite.hx
+++ src/cncta/inject/ui/Composite.hx
@@ -3,9 +3,9 @@ package cncta.inject.ui;
 @:native("qx.ui.container.Composite")
 extern class Composite
 {
-    public function getChildren():List<LayoutItem>;
+    public function getChildren():Array<LayoutItem>;
 
-    public function addAt(child:LayoutItem, index):Void;
+    public function addAt(child:LayoutItem, index:Int):Void;
     public function addBefore(child:LayoutItem, before:LayoutItem):Void;
     public function addAfter(child:LayoutItem, after:LayoutItem):Void;
 }
