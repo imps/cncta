c1e87425d4747f61be672f0d4b1f31f84192f732
aszlig <aszlig@redmoonstudios.org>
2012-04-03 20:44:15 +0200

    Add missing @:native metadata to externs.
    These were non-trivial as they're retrieved only using get_*() methods from
    parent instances, but it might lead to confusion if we want to instantiate them
    some day.

--- src/cncta/inject/data/Chat.hx
+++ src/cncta/inject/data/Chat.hx
@@ -1,5 +1,6 @@
 package cncta.inject.data;
 
+@:native("ClientLib.Data.Chat")
 extern class Chat
 {
     public dynamic function AddMsg(t:String):Void;
--- src/cncta/inject/data/Server.hx
+++ src/cncta/inject/data/Server.hx
@@ -1,5 +1,6 @@
 package cncta.inject.data;
 
+@:native("ClientLib.Data.Server")
 extern class Server
 {
     public function get_WorldId():Int;
