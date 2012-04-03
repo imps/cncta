88ad4676e36fd10d605e75a6c7f1e060b53b90eb
aszlig <aszlig@redmoonstudios.org>
2012-04-03 03:38:09 +0200

    Add missing external UI declarations.

--- src/cncta/inject/qx/Widget.hx
+++ src/cncta/inject/qx/Widget.hx
@@ -3,5 +3,6 @@ package cncta.inject.qx;
 @:native("qx.ui.core.Widget")
 extern class Widget extends LayoutItem
 {
-    public function setVisibility(v:String):String;
+    public function setVisibility(v:String):Void;
+    public function setDecorator(v:String):Void;
 }
--- src/cncta/inject/qx/Window.hx
+++ src/cncta/inject/qx/Window.hx
@@ -4,6 +4,7 @@ package cncta.inject.qx;
 extern class Window extends Widget
 {
     public function new(title:String, ?icon:String):Void;
+    public function add(child:LayoutItem, ?options:Dynamic):Widget;
     public function show():Void;
     public function setAppearance(value:String):String;
 }
