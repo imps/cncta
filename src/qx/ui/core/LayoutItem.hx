238d80d9cf2d9fd183412743895a4ed24161cb81
aszlig <aszlig@redmoonstudios.org>
2012-04-04 07:15:25 +0200

    Add getLayout and setLayout to LayoutItem.
    In Qooxdoo this is a mixin class, but for our case we add it directly to
    LayoutItem. At least it makes sense for now, as we really don't need mixins,
    do we?

--- src/qx/ui/core/LayoutItem.hx
+++ src/qx/ui/core/LayoutItem.hx
@@ -2,4 +2,6 @@ package qx.ui.core;
 
 extern class LayoutItem extends qx.core.Object
 {
+    public function getLayout():qx.ui.layout.Abstract;
+    public function setLayout(layout:qx.ui.layout.Abstract):Void;
 }
