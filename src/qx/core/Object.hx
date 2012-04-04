a4bfb4e3314318e87adb31b769ca62fbecb92fe1
aszlig <aszlig@redmoonstudios.org>
2012-04-04 10:29:01 +0200

    Make argument 2 of Object.addListener Dynamic.
    This is because the argument to the event listener function can either be an
    event object of any kind or no argument at all.

--- src/qx/core/Object.hx
+++ src/qx/core/Object.hx
@@ -3,5 +3,5 @@ package qx.core;
 extern class Object
 {
     public function set(data:Dynamic):Object;
-    public function addListener(type:String, listener:Void -> Void):String;
+    public function addListener(type:String, listener:Dynamic):String;
 }
