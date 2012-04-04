61b9be4aadd1e67728335beed068892e58887d48
aszlig <aszlig@redmoonstudios.org>
2012-04-04 10:43:39 +0200

    Set window properties for ChatWindow.
    Which includes adding the ChatInput widget to the VBox container.

--- src/cncta/ui/ChatWindow.hx
+++ src/cncta/ui/ChatWindow.hx
@@ -5,5 +5,21 @@ class ChatWindow extends cncta.inject.ui.CustomWindow
     public function new()
     {
         super("Chat");
+
+        this.setLayout(new qx.ui.layout.VBox());
+
+        this.set({
+            allowMaximize: true,
+            allowMinimize: false,
+            showMaximize: true,
+            showMinimize: false,
+            showStatusbar: false,
+            resizable: true,
+            movable: true,
+            alwaysOnTop: false,
+            showClose: true,
+        });
+
+        this.add(new cncta.ui.chat.ChatInput());
     }
 }
