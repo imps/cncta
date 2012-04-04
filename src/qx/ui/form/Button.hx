d7dbf50390075d30806653010b85d0e9edc92d81
aszlig <aszlig@redmoonstudios.org>
2012-04-04 10:31:29 +0200

    Second argemunt of Button constructor is optional.

--- src/qx/ui/form/Button.hx
+++ src/qx/ui/form/Button.hx
@@ -2,5 +2,5 @@ package qx.ui.form;
 
 extern class Button extends qx.ui.basic.Atom
 {
-    public function new(label:String, icon:String):Void;
+    public function new(label:String, ?icon:String):Void;
 }
