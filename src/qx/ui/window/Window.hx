25ee00629716692b861c5ddea97ac18428ebcf72
aszlig <aszlig@redmoonstudios.org>
2012-04-04 06:03:33 +0200

    Add/correct missing binds for visibility control.
    The show() method really is from qx.ui.core.Widget and not from Window, so we're
    correcting it here. In addition we add some other bindings related to visibility
    control.

--- src/qx/ui/window/Window.hx
+++ src/qx/ui/window/Window.hx
@@ -7,5 +7,7 @@ extern class Window extends Widget
 {
     public function new(title:String, ?icon:String):Void;
     public function add(child:LayoutItem, ?options:Dynamic):Widget;
-    public function show():Void;
+
+    public function close():Void;
+    public function open():Void;
 }
