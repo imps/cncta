0517d3db549d3b7b411324b29b81e6a98f5d8a00
aszlig <aszlig@redmoonstudios.org>
2012-04-04 10:33:09 +0200

    Add bindings for qx.ui.form.TextField.
    This also involves the addition of the superclass AbstractField.

---
+++ src/qx/ui/form/AbstractField.hx
@@ -0,0 +1,7 @@
+package qx.ui.form;
+
+extern class AbstractField extends qx.ui.core.Widget
+{
+    public function getValue():String;
+    public function setValue(value:String):Void;
+}
---
+++ src/qx/ui/form/TextField.hx
@@ -0,0 +1,6 @@
+package qx.ui.form;
+
+extern class TextField extends qx.ui.form.AbstractField
+{
+    public function new(?value:String):Void;
+}
