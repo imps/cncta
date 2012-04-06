ef41f4e13801a5eecb79a8953b5a625812e0626d
aszlig <aszlig@redmoonstudios.org>
2012-04-06 16:20:38 +0100

    Get rid of obsolete cruft in xmpp.ChatMessage.
    This was only relevant for injecting message types into the C&C TA API, and as
    we now have our own chat window, we don't really need this anymore.

--- src/cncta/xmpp/ChatMessage.hx
+++ src/cncta/xmpp/ChatMessage.hx
@@ -2,30 +2,14 @@ package cncta.xmpp;
 
 class ChatMessage
 {
-    public var content:String;
     public var sender:String;
     public var date:Date;
     public var message:String;
 
-    //c = "@A";         // alliance
-    //c = "@C";         // dunno?
-    //c = "@CCC";       // dunno?
-    //c = "@CCM";       // dunno?
-    //s = "@System";    // system message
-    //s = "@Info";      // info message
-    //c = "privatein";  // incoming private message
-    //c = "privateout"; // outgoing private message
-
     public function new(sender:String, date:Date, msg:String)
     {
-        this.content = "@A";
         this.sender = sender;
         this.date = date;
         this.message = msg;
-    }
-
-    public function get_object():Dynamic
-    {
-        return {c: this.content, s: this.sender, m: this.message};
     }
 }
