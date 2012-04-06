66f182f69cd6b3b00c0ab2525cb41259666ea528
aszlig <aszlig@redmoonstudios.org>
2012-04-06 14:04:43 +0100

    Add missing scrollbar stuff to QX bindings.

---
+++ src/qx/ui/container/Scroll.hx
@@ -0,0 +1,6 @@
+package qx.ui.container;
+
+extern class Scroll extends qx.ui.core.scroll.AbstractScrollArea
+{
+    public function new(?content:qx.ui.core.LayoutItem):Void;
+}
---
+++ src/qx/ui/core/scroll/AbstractScrollArea.hx
@@ -0,0 +1,7 @@
+package qx.ui.core.scroll;
+
+extern class AbstractScrollArea extends qx.ui.core.Widget
+{
+    public function scrollToX(value:Int):Void;
+    public function scrollToY(value:Int):Void;
+}
