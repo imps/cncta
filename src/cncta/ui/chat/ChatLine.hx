650c05ece7ab452d6881467f8e0b8d416d746a50
aszlig <aszlig@redmoonstudios.org>
2012-04-08 11:11:43 +0100

    Split ChatLine into several widgets.
    This really helps in alignment of text to the right of the nickname and not flow
    into the next line anymore.

--- src/cncta/ui/chat/ChatLine.hx
+++ src/cncta/ui/chat/ChatLine.hx
@@ -1,23 +1,36 @@
 package cncta.ui.chat;
 
-class ChatLine extends qx.ui.basic.Label
+class ChatLine extends qx.ui.container.Composite
 {
     public function new(date:Date, nick:String, text:String, is_me:Bool)
     {
+        super(new qx.ui.layout.HBox(5));
+
         var time = DateTools.format(date, "%H:%M:%S");
 
         var bbtext = cncta.inject.Util.convertBBCode(text, true, "#ff9900");
 
-        var fullmsg = this.color("#00ff00", "[" + time + "]");
-        fullmsg += " " + this.color(is_me ? "#ff5050" : "#66ffff", nick);
-        fullmsg += this.color("#00cc00", ":");
-        fullmsg += " " + this.color("#ffffff", bbtext);
+        var timelabel = new cncta.ui.ColorLabel("#00ff00", "[" + time + "]");
+        timelabel.set({selectable: true});
+        this.add(timelabel);
 
-        super(fullmsg);
-        this.set({
+        var nicklabel = new qx.ui.basic.ColorLabel(
+            is_me ? "#ff5050" : "#66ffff",
+            nick + ":"
+        );
+        nicklabel.set({selectable: true});
+        this.add(nicklabel);
+
+        var textlabel = new cncta.ui.ColorLabel("#ffffff", bbtext);
+
+        textlabel.set({
             rich: true,
+            wrap: true,
             selectable: true,
+            allowGrowY: true,
         });
+
+        this.add(textlabel, {flex: 1});
     }
 
     private function color(color:String, value:String)
