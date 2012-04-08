f35dd5afc3232d49b27185cc4e411269317cf16c
aszlig <aszlig@redmoonstudios.org>
2012-04-08 06:30:07 +0100

    Add theming methods to Qooxdoo bindings.
    We will need to use them in order to set/reset decorators or appearance.

--- src/qx/ui/core/Widget.hx
+++ src/qx/ui/core/Widget.hx
@@ -3,10 +3,19 @@ package qx.ui.core;
 extern class Widget extends LayoutItem
 {
     public function setVisibility(v:String):Void;
-    public function setDecorator(v:String):Void;
 
+    public function getDecorator():Dynamic;
+    public function setDecorator(v:Dynamic):Void;
+    public function resetDecorator():Void;
+
+    public function getAppearance():String;
     public function setAppearance(v:String):String;
     public function updateAppearance():Void;
+
+    public function getBackgroundColor():Dynamic;
+    public function setBackgroundColor(value:Dynamic):Void;
+
+    public function syncWidget():Void;
 
     public function show():Void;
     public function hide():Void;
